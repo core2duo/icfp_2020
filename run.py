d19a39ae0b806595e15fb9e65e9c1f84f090a6da
kmeaw <kmeaw@kmeaw.com>
2020-07-18 05:05:43 +0300

    Implement B,C,S,I, boolean and integer arithmetic

--- run.py
+++ run.py
@@ -12,7 +12,139 @@ class Number(Atom):
         self.value = int(value)
 
     def eval(self):
-        return self.value, words
+        return self.value
+
+
+class TrueAtom(Atom):
+    arity = 2
+
+    def __init__(self, name=''):
+        self.name = 't'
+
+    def load(self, bt, tf):
+        self.bt = bt
+        self.bf = bf
+
+    def eval(self):
+        return self.bt
+
+    def __repr__(self):
+        return '{!r} {!r} {!r}'.format(self.name, self.bt, self.bf)
+
+
+class FalseAtom(TrueAtom):
+    def eval(self):
+        return self.bf
+
+    def __init__(self, name=''):
+        self.name = 'f'
+
+
+class Add(Atom):
+    arity = 2
+
+    def __init__(self, name=''):
+        self.name = 'add'
+
+    def load(self, a, b):
+        self.a = a
+        self.b = b
+
+    def eval(self):
+        return self.a.eval() + self.b.eval()
+
+    def __repr__(self):
+        return '{!r} + {!r}'.format(self.a, self.b)
+
+
+class Mul(Atom):
+    arity = 2
+
+    def __init__(self, name=''):
+        self.name = 'mul'
+
+    def load(self, a, b):
+        self.a = a
+        self.b = b
+
+    def eval(self):
+        return self.a.eval() * self.b.eval()
+
+    def __repr__(self):
+        return '{!r} * {!r}'.format(self.a, self.b)
+
+
+class Div(Atom):
+    arity = 2
+
+    def __init__(self, name=''):
+        self.name = 'div'
+
+    def load(self, a, b):
+        self.a = a
+        self.b = b
+
+    def eval(self):
+        return self.a.eval() / self.b.eval()
+
+    def __repr__(self):
+        return '{!r} / {!r}'.format(self.a, self.b)
+
+
+class Eq(Atom):
+    arity = 2
+
+    def __init__(self, name=''):
+        self.name = 'eq'
+
+    def load(self, a, b):
+        self.a = a
+        self.b = b
+
+    def eval(self):
+        if self.a.eval() == self.b.eval():
+            return TrueAtom('t')
+        else:
+            return FalseAtom('f')
+
+    def __repr__(self):
+        return '{!r} == {!r}'.format(self.a, self.b)
+
+
+class Lt(Atom):
+    arity = 2
+
+    def __init__(self, name=''):
+        self.name = 'lt'
+
+    def load(self, a, b):
+        self.a = a
+        self.b = b
+
+    def eval(self):
+        if self.a.eval() < self.b.eval():
+            return TrueAtom('t')
+        else:
+            return FalseAtom('f')
+
+    def __repr__(self):
+        return '{!r} < {!r}'.format(self.a, self.b)
+
+
+class I(Atom):
+    arity = 1
+
+    def __init__(self, name=''):
+        self.name = 'i'
+
+    def load(self, value):
+        self.value = value
+
+    def eval(self):
+        return self.value.eval()
+
+    def __repr__(self):
+        return '{!r}'.format(self.value)
 
 
 class Nil(Atom):
@@ -33,7 +165,6 @@ class Neg(Atom):
 
     def __init__(self, name=''):
         self.name = 'neg'
-        self.value = None
 
     def load(self, value):
         self.value = value
@@ -51,7 +182,7 @@ class Thunk(object):
         self.args = args or []
 
     def __repr__(self):
-        return '{}/{}({})'.format(self.kind, len(self.args), ', '.join(map(repr, self.args)))
+        return '{}/{} ({})'.format(self.kind, len(self.args), ', '.join(map(repr, self.args)))
 
     def push(self, value):
         self.args.append(value)
@@ -73,6 +204,7 @@ class Parser(object):
         for word in words:
             if word == 'ap':
                 fun = self.stack.pop()
+                assert len(self.stack) > 0, 'stack underflow for {!r}'.format(fun)
                 arg = self.stack.pop()
                 if isinstance(fun, type) and issubclass(fun, Atom):
                     self.stack.append(Thunk(fun).push(arg))
@@ -88,6 +220,8 @@ class Parser(object):
                     self.stack.append(special)
             elif word in env:
                 self.stack.append(env[word])
+            elif word.startswith('-') and word[1:].isnumeric():
+                self.stack.append(Number(word))
             elif word.isnumeric():
                 self.stack.append(Number(word))
             else:
@@ -130,10 +264,140 @@ class Cons(Atom):
         return value
 
 
+class C(Atom):
+    arity = 3
+
+    def __init__(self, name=''):
+        self.name = 'C'
+
+    def load(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def eval(self):
+        x = self.x.eval()
+        z = self.z.eval()
+        y = self.y.eval()
+
+    def __repr__(self):
+        return '{!r} ({!r}) ({!r})'.format(self.x, self.z, self.y)
+
+
+class B(Atom):
+    arity = 3
+
+    def __init__(self, name=''):
+        self.name = 'B'
+
+    def load(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def eval(self):
+        x = self.x.eval()
+        y = self.y.eval()
+        z = self.z.eval()
+
+    def __repr__(self):
+        return '{!r} ({!r} ({(!r}))'.format(self.x, self.y, self.z)
+
+
+class S(Atom):
+    arity = 3
+
+    def __init__(self, name=''):
+        self.name = 'S'
+
+    def load(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def eval(self):
+        x = self.x.eval()
+        y = self.y.eval()
+        z = self.z.eval()
+
+    def __repr__(self):
+        return 'S({!r}, {!r}, {!r})'.format(self.x, self.y, self.z)
+
+
+class Car(Atom):
+    arity = 1
+
+    def __init__(self, name=''):
+        self.name = 'car'
+
+    def load(self, cons):
+        self.cons = cons
+
+    def eval(self):
+        cons = self.cons.eval()
+        assert isinstance(cons, Cons)
+        return cons.car
+
+    def __repr__(self):
+        return 'car ({!r})'.format(self.cons)
+
+
+class Cdr(Atom):
+    arity = 1
+
+    def __init__(self, name=''):
+        self.name = 'cdr'
+
+    def load(self, cons):
+        self.cons = cons
+
+    def eval(self):
+        cons = self.cons.eval()
+        assert isinstance(cons, Cons)
+        return cons.cdr
+
+    def __repr__(self):
+        return 'cdr ({!r})'.format(self.cons)
+
+
+class IsNil(Atom):
+    arity = 1
+
+    def __init__(self, name=''):
+        self.name = 'isnil'
+
+    def load(self, cons):
+        self.cons = cons
+
+    def eval(self):
+        cons = self.cons.eval()
+        if isinstance(cons, Cons):
+            return SPECIALS['t']('t')
+        else:
+            return SPECIALS['f']('f')
+
+    def __repr__(self):
+        return 'isnil ({!r})'.format(self.cons)
+
+
 SPECIALS = {
              'cons': Cons,
              'nil': Nil,
              'neg': Neg,
+             'c': C,
+             'b': B,
+             'car': Car,
+             'cdr': Cdr,
+             't': TrueAtom,
+             'f': FalseAtom,
+             'isnil': IsNil,
+             's': S,
+             'add': Add,
+             'mul': Mul,
+             'eq': Eq,
+             'div': Div,
+             'lt': Lt,
+             'i': I,
            }
 
 env = {
@@ -150,6 +414,10 @@ with open('galaxy.txt') as f:
 for ident, atom in env.items():
     if type(atom) is Atom:
         parser = Parser(env)
-        code = parser.parse(atom.value)
+        try:
+            code = parser.parse(atom.value)
+        except:
+            print('Error parsing', ident)
+            raise
         env[ident] = code
         print('{} = {!r}'.format(ident, code))
